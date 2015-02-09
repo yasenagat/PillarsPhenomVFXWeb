7cc9978c178d83ea3c185e6321d9a6b58b212ab6
yupengfei <yupengfei@foxmail.com>
2015-02-09 22:58:25 +0800

    finish r3d

--- r3dOperation/r3dOperation_test.go
+++ r3dOperation/r3dOperation_test.go
@@ -3,8 +3,43 @@ package r3dOperation
 import (
 	"fmt"
 	"testing"
+	"os"
 )
 
 func TestBuildDate(t * testing.T) {
 	fmt.Println(BuildDate())
+}
+
+	// Clip ClipInit(const char *);
+	// //int ClipStatus();
+	// int ClipVideoTrackCount(Clip);
+	// int ClipWidth(Clip);
+	// int ClipHeight(Clip);
+	// float ClipVideoAudioFramerate(Clip);
+	// float ClipTimecodeFramerate(Clip);
+	// int ClipVideoFrameCount(Clip);
+	// const char * ClipStartAbsoluteTimecode(Clip);
+	// const char * ClipEndAbsoluteTimecode(Clip);
+	// const char * ClipStartEdgeTimecode(Clip);
+	// const char * ClipEndEdgeTimecode(Clip);
+	// void ClipFree(Clip);
+func TestClip(t * testing.T) {
+	clip := ClipInit("/home/developer/Videos/B011_C010_060551.RDC/B011_C010_060551_002.R3D")
+	fmt.Println(ClipVideoTrackCount(clip))
+	fmt.Println(ClipWidth(clip))
+	fmt.Println(ClipHeight(clip))
+	fmt.Println(ClipVideoAudioFramerate(clip))
+	fmt.Println(ClipTimecodeFramerate(clip))
+	fmt.Println(ClipVideoFrameCount(clip))
+	fmt.Println(ClipStartAbsoluteTimecode(clip))
+	//fmt.Println(ClipEndAbsoluteTimecode(clip))
+	fmt.Println(ClipStartEdgeTimecode(clip))
+	fmt.Println(ClipEndEdgeTimecode(clip))
+	fmt.Println(ClipMetaData(clip))
+	file, _ := os.Create("test.png")
+	defer file.Close()
+	ClipDecodeFrame(clip, 1000, file)
+
+	//ClipDecodeFrameFree(buffer)
+	ClipFree(clip)
 }
