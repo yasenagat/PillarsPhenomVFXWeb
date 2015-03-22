fa32e60d3304d581c406fa4885aca13a86ba0b91
chengxuezhi <78131560@qq.com>
2015-03-22 14:28:34 +0800

    Add post html function

--- controller/postAction/shotVersionAction.go
+++ controller/postAction/shotVersionAction.go
@@ -48,6 +48,37 @@ func AddShotDemo(w http.ResponseWriter, r *http.Request) {
 	u.OutputJsonLog(w, 0, "Add success.", nil, "")
 }
 
+func QueryShotVersion(w http.ResponseWriter, r *http.Request) {
+	flag, _ := s.GetAuthorityCode(w, r, "制片")
+	if !flag {
+		u.OutputJsonLog(w, 404, "session error!", nil, "")
+		return
+	}
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		u.OutputJsonLog(w, 1, "Read body failed!", nil, "postAction.QueryShotVersion: ioutil.ReadAll(r.Body) failed!")
+		return
+	}
+	var version u.ShotVersion
+	json.Unmarshal(data, &version)
+	if err != nil {
+		u.OutputJsonLog(w, 12, err.Error(), nil, "postAction.QueryShotVersion: json.Unmarshal(data, &version) failed!")
+		return
+	}
+	if len(version.ShotCode) == 0 {
+		u.OutputJsonLog(w, 13, "Parameters Checked failed!", nil, "postAction.QueryShotVersion: Parameters Checked failed!")
+		return
+	}
+	result, err := postStorage.QueryShotVersion(&version.ShotCode)
+	if err != nil || result == nil {
+		u.OutputJsonLog(w, 15, "Query failed!", nil, "postAction.QueryShotVersion: postStorage.QueryShotVersion(&ShotCode) failed!")
+		return
+	}
+
+	u.OutputJsonLog(w, 0, "Query success.", nil, "")
+}
+
 //func DeleteVersion(w http.ResponseWriter, r *http.Request) {
 //	data, err := ioutil.ReadAll(r.Body)
 //	if err != nil {
@@ -64,21 +95,4 @@ func AddShotDemo(w http.ResponseWriter, r *http.Request) {
 //		return
 //	}
 //	u.OutputJson(w, 0, "Delete success.", nil)
-//}
-//func QueryVersionByShot(w http.ResponseWriter, r *http.Request) {
-//	data, err := ioutil.ReadAll(r.Body)
-//	if err != nil {
-//		u.OutputJson(w, 1, "Read body failed!", nil)
-//		//pillarsLog.PillarsLogger.Print("ioutil.ReadAll(r.Body) failed!")
-//		return
-//	}
-//	var code string
-//	json.Unmarshal(data, &code)
-//	result, err := postStorage.QueryVersionByShot(&code)
-//	if err != nil {
-//		u.OutputJson(w, 2, "Read body failed!", nil)
-//		//pillarsLog.PillarsLogger.Print("ioutil.ReadAll(r.Body) failed!")
-//		return
-//	}
-//	u.OutputJson(w, 0, "Query success.", result)
 //}
--- storage/postStorage/shotVersionOperation.go
+++ storage/postStorage/shotVersionOperation.go
@@ -30,6 +30,29 @@ func GetShotVersionNum(sv *utility.ShotVersion) (*int, error) {
 	return &n, nil
 }
 
+func QueryShotVersion(shotCode *string) (*[]utility.ShotVersion, error) {
+	stmt, err := mysqlUtility.DBConn.Prepare("SELECT version_code, version_num, picture, demo_datail FROM shot_version WHERE status = 0 AND shot_code = ? ORDER BY version_num DESC")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	result, err := stmt.Query(shotCode)
+	if err != nil {
+		return nil, err
+	}
+	defer result.Close()
+	var versions []utility.ShotVersion
+	for result.Next() {
+		var version utility.ShotVersion
+		err = result.Scan(&version.VersionCode, &version.VersionNum, &version.Picture, &version.DemoDetail)
+		if err != nil {
+			return nil, err
+		}
+		versions = append(versions, version)
+	}
+	return &versions, nil
+}
+
 //func DeleteShotVersion(code *string) error {
 //	stmt, err := mysqlUtility.DBConn.Prepare("UPDATE shot_version SET status = 1 WHERE status = 0 AND version_code = ?")
 //	defer stmt.Close()
@@ -55,27 +78,4 @@ func GetShotVersionNum(sv *utility.ShotVersion) (*int, error) {
 //		return nil, err
 //	}
 //	return &version, nil
-//}
-
-//func QueryShotVersionByShot(code *string) ([]utility.ShotVersion, error) {
-//	stmt, err := mysqlUtility.DBConn.Prepare("SELECT version_code,shot_code,vendor_code,thumbnail,demo,product,status FROM shot_version WHERE shot_code=?  AND status=0")
-//	defer stmt.Close()
-//	var versions []utility.ShotVersion
-//	if err != nil {
-//		return versions, err
-//	}
-//	result, err := stmt.Query(code)
-//	defer result.Close()
-//	if err != nil {
-//		return versions, err
-//	}
-//	if result.Next() {
-//		var shot_version utility.ShotVersion
-//		err = result.Scan(&version.VersionCode, &version.ShotCode, &version.VendorCode, &version.Image, &version.Demo, &version.Product, &version.Status)
-//		if err != nil {
-//			return versions, err
-//		}
-//		versions = append(versions, version)
-//	}
-//	return versions, nil
 //}
